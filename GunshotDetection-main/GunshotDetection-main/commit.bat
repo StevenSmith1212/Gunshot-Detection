720a57f484a5017d981bf1013e45b03ab9cb3e81
Steven Smith <smit3841@pnw.edu>
2023-02-28 09:34:33 -0600

    initial code commit

---
+++ GunshotDetection-main/GunshotDetection-main/commit.bat
@@ -0,0 +1,9 @@
+
+cd C:\Users\swsmi\OneDrive\Documents\Gunshot-Detection\GunshotDetection-main\GunshotDetection-main
+git add index.html
+git add SystemSounds\
+git commit -m "gunshot detected"
+git push origin main
+
+
+

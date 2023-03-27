fe52f06e62b01f12131cbfc92936e91de8123966
Bruno Hnatusko III <bhnatusk@pnw.edu>
2023-03-27 16:14:28 -0500

    update

--- GunshotDetection-main/GunshotDetection-main/commit.bat
+++ GunshotDetection-main/GunshotDetection-main/commit.bat
@@ -1,10 +1,10 @@
 
-cd C:\Users\swsmi\OneDrive\Documents\Gunshot-Detection\GunshotDetection-main\GunshotDetection-main
+cd C:\Users\Bruno\OneDrive\Documents\Gunshot-Detection\GunshotDetection-main\GunshotDetection-main
 git fetch
 git add index.html
 git add SystemSounds\
 git commit -m "gunshot detected"
-git push origin main
+git push origin Lawshe
 
 
 
